0ca84df4bb30caffc3fed571edfbf8366f8c5f5d
Samuel Rechsteiner <samrech@umich.edu>
2026-08-10 14:21:09 -0500

    remove gitignore files

--- advanced-design/dynamic-programming/bitonic-tour/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include "bitonic.c"
-
-int main() {
-    point p[] = { {0,6}, {1,0}, {2,3}, {5,4}, {6,1}, {7,5}, {8,2} };
-    int n = 6;
-    printf("%02f\n", bitonic_length_recursive(p, n - 1));
-}
--- advanced-design/dynamic-programming/longest-common-increasing-subsequence/main.c
+++
@@ -1,8 +0,0 @@
-#include "lcis.c"
-
-int main() {
-    int A[] = { 5, 9, 11, 4, 12, 7, 2, 18, 4 };
-    longest_monotonic(A, 9);
-    // int A[] = { 2, 4, 4, 5, 7, 9, 11, 12, 18 };
-    // printf("%d\n", A[last_smaller_elt_index(A, 20, sizeof(A)/sizeof(int))]);
-}
